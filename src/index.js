c305a43c2b7890472eaf850451f02ae96b71d3a7
Sergey <44573680+pjonone@users.noreply.github.com>
2019-03-18 17:51:08 +0300

    fix: delete check for close

--- src/index.js
+++ src/index.js
@@ -5,23 +5,14 @@ module.exports = function check(str, bracketsConfig) {
         open.push(bracketsConfig[i][0]);
         close.push(bracketsConfig[i][1]);
     }
-
-let stack = [];
-for (let s = 0; s < str.length; s++) {
-
-    if (open.includes(str.charAt(s)) === true) { // проверяю есть ли в открытых элемент из строки, если есть пушу в стек
-        stack.push(str.charAt(s));
-    }
-    if (close.includes(str.charAt(s)) === true) { // проверяю есть ли в закрытых, если есть то
-        if (open[close.indexOf(str.charAt(s))] === stack[stack.length - 1]) { // беру номер позиции закрытого элемента и сравниванию позицию в открытом под этим номером с последним пушем если оно === то делаем поп
+    let stack = [];
+    for (let s = 0; s < str.length; s++) {
+        if (open[close.indexOf(str[s])] === stack[stack.length - 1] && stack.length > 0) {
             stack.pop();
         }
+        else {
+            stack.push(str[s]);
+        }
     }
-
-}
-
-    if (stack.length === 0) { return true;}
-     else {return false;}
-
-
+    return (stack.length > 0) ? false : true;
 }
